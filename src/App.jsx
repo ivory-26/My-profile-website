d5cfbb627cb2f81b3b4a10bd3b065fd8d808f1cb
Sahil Karpe <167101357+ivory-26@users.noreply.github.com>
2024-12-13 07:30:11 +0000

    updated about me

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Sahil Karpe",
+  title: "Student trying to code",
+  email: "karpesahil2007@gmail.com",
+  gitHub: "ivory-26",
+  instagram: "sahil_26.01",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "@Sahil_Karpe07",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
